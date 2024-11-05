24da74539fbeaff87296750ffdec393ee5938f0d
ohiduzzamansiam1 <siam500561@gmail.com>
2024-11-05 23:20:10 +0600

    Refactor page component to remove unused code and improve performance

--- app/page.tsx
+++ app/page.tsx
@@ -2,30 +2,19 @@
 
 import { WebsitesList } from "@/components/WebsitesList";
 import { api } from "@/convex/_generated/api";
-import { Analytics } from "@/lib/analytics";
 import { useQuery } from "convex/react";
-import { useEffect } from "react";
-
-const analytics = new Analytics("wa_6H5Te6SAzKORuOlzTK4ryQV1uV8tNi3F");
 
 export default function Home() {
   const websites = useQuery(api.websites.list);
-
-  useEffect(() => {
-    analytics
-      .trackPageView()
-      .then(() => console.log("Page view tracked"))
-      .catch((error: Error) =>
-        console.error("Error tracking page view:", error)
-      );
-  }, []);
 
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-8">Web Analytics Dashboard</h1>
       <div className="grid grid-cols-1 gap-8">
-        {/* <AddWebsiteForm /> */}
-        <WebsitesList websites={websites || []} />
+        <WebsitesList
+          isLoading={websites === undefined}
+          websites={websites || []}
+        />
       </div>
     </main>
   );
